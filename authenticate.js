f80ac4bc6372ce52ed8eb5f5118d8701dbf37113
Ashraf <deva4rf@gmail.com>
2020-09-11 17:03:35 -0400

    Updated README.md

--- authenticate.js
+++ authenticate.js
@@ -22,7 +22,7 @@ var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secret ;
 exports.JWTpassport = passport.use( new JwtStrategy( opts, function( jwt_payload, done ) {
-	User.findOne( {_id: jwt_payload._id }, function( err, user ) {
+	User.findOne( { _id: jwt_payload._id }, function( err, user ) {
 		if ( err ) {
 			return done( err, false );
 		}
